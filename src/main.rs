e3821547d956ab39735a52e1d4cb21330eb6caad
Priyanshu Soni <priyanshu171203@gmail.com>
2026-01-18 15:10:25 +0530

    added checksumming

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader, Seek, SeekFrom, Write};
 use std::io::Error;
 use std::io::ErrorKind;
+use std::mem::forget;
+use crc32fast::Hasher;
+
 fn main() {
     println!("WAL application for learning basic rust");
 }
@@ -9,16 +12,19 @@ fn main() {
 #[derive(Debug)]
 struct Log {
     key: String,
-    value: String,
-    checksum: u32
+    value: String
 }
 
 impl Log {
     fn write_data(&self,file: &mut File) -> Result<usize, std::io::Error> {
-        let record = format!("{}\n{}\n{}\n",self.key, self.value, self.checksum);
-        let bytes = record.as_bytes();
-        file.write_all(bytes)?;
-        Ok(bytes.len())
+        let data = format!("{}\n{}\n",self.key,self.value);
+        let data_bytes = data.as_bytes();
+        let mut hasher = Hasher::new();
+        hasher.update(data_bytes);
+        let checksum = hasher.finalize();
+        file.write_all(data_bytes)?;
+        file.write_all(format!("{}\n", checksum).as_bytes())?;
+        Ok(data_bytes.len() + checksum.to_string().len() + 1)
     }
 
     fn read_data(file: &mut File) -> Result<Log, Error> {
@@ -35,11 +41,23 @@ impl Log {
         }
         reader.read_line(&mut value)?;
         reader.read_line(&mut checksum_str)?;
-        let checksum = checksum_str.trim().parse().expect("invalid checksum");
+        let checksum_int :u32 = checksum_str.trim().parse().expect("Unable to parse checksum");
+        let data = format!("{}{}", key, value);
+        let data_bytes = data.as_bytes();
+        let mut hasher = Hasher::new();
+        hasher.update(data_bytes);
+        let checksum = hasher.finalize();
+        println!("Checksum is {}",checksum);
+        println!("Stored Checksum is {}",checksum_int);
+        if checksum_int != checksum {
+            return Err(Error::new(
+                ErrorKind::InvalidData,
+                "checksum mismatch",
+            ))
+        }
         Ok(Log{
             key,
-            value,
-            checksum
+            value
         })
     }
 }
@@ -59,7 +77,6 @@ mod tests {
         let log = Log {
             key : String::from("k1"),
             value : String::from("v1"),
-            checksum : 1
         };
         let bytes_written = log.write_data(&mut file).expect("write failed");
         assert!(bytes_written > 0);
@@ -78,13 +95,13 @@ mod tests {
         let log = Log {
             key : String::from("k1"),
             value : String::from("v1"),
-            checksum : 1
         };
         log.write_data(&mut file).expect("write failed");
         file.seek(SeekFrom::Start(0)).expect("seek failed");
         Log::read_data(&mut file).expect("read failed");
         assert_eq!(log.key, "k1");
         assert_eq!(log.value, "v1");
-        assert_eq!(log.checksum, 1);
     }
+
+
 }
