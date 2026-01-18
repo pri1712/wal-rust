21d9a6b9962ec59c5658ee169a016daccc6eeefa
Priyanshu Soni <priyanshu171203@gmail.com>
2026-01-18 11:24:25 +0530

    wal for rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+
 fn main() {
     println!("WAL application for learning basic rust")
 
@@ -6,5 +8,18 @@ fn main() {
 struct Log {
     Key: String,
     Value: String,
+    Checksum: u32
+}
 
+impl Log {
+    fn write_data(&self,file: &mut File) {
+        //write data to the end of the file and increment the file offset.
+        //on successful write return the number of bytes written, else return -1.
+
+    }
+
+    fn read_data(&self, file: &File, offset: isize) -> Log {
+        //read the data from file at the given offset. calculate checksum of
+        // data, if it does not match the checksum stored, return -1
+    }
 }
